65ab4996861b33c147caf396b452638520d4887f
annabananna <ana_bogdanovska@yahoo.com>
2023-03-07 20:53:30 +0100

    Add files via upload

---
+++ Homeworks/Exercise03HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise03HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare an integer and a string variable
+            //Initialize them and concatenate them in a new variable
+            //Print the result in the console
+
+            int number = 10;
+            string word = "homework";
+            string result = number .ToString() + word ;
+            //string result2 = number + word;
+            Console.WriteLine(result);
+        }
+    }
+}
