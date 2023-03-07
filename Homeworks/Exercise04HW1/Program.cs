ef46add019671fa027fd4f82793f856d51cafc38
annabananna <ana_bogdanovska@yahoo.com>
2023-03-07 20:53:49 +0100

    Add files via upload

---
+++ Homeworks/Exercise04HW1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace Exercise04HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise04");
+            //You have n credits on your mobile bill. One SMS costs m credits.
+            //How many SMS messages you can send ?
+            //n = 102
+            //m = 5
+            //Result = ?
+
+            int credits = 102;
+            int smsToCredit = 5;
+
+            int result = credits / smsToCredit;
+            Console.WriteLine(result);
+
+        }
+    }
+}
