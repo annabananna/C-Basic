31c5d6e8ca68f7cc88be4a37ec9f00cf3cecde8c
annabananna <ana_bogdanovska@yahoo.com>
2023-03-07 20:54:25 +0100

    Add files via upload

---
+++ Homeworks/Task1 HW2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Class_2_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Real calculator");
+            // Create new console application “RealCalculator” that takes two numbers from the input and asks what operation 
+            // would the user want to be done( +, - , * , / ). Then it returns the result.
+
+            Console.WriteLine("Enter the first number");
+            string firstNumber = Console.ReadLine();
+            if(!double.TryParse(firstNumber, out double number1))
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+
+            Console.WriteLine("Enter the second number");
+            if(!double.TryParse(Console.ReadLine(),out double number2)) {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+
+            Console.WriteLine("Enter the operation");
+            string op = Console.ReadLine();
+
+            if(op == "+")
+            {
+                Console.WriteLine(number1 + number2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(number1 - number2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(number1 * number2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(number1 / number2);
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid operation");
+            }
+
+        }
+    }
+}
