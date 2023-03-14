a247de99e93ac187eef2141f402875c99ac613ca
annabananna <ana_bogdanovska@yahoo.com>
2023-03-14 22:56:23 +0100

    Add files via upload

---
+++ Homeworks/Class04HW_Bonus/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Class04HW_Bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bonus
+            //Take a sentence as input and print its largest word
+
+            string sentence = "Hello from SEDC academy, wish you lot's of luck this semester!";
+            string[] words = sentence.Split(' ', ',', '!');
+            int wordsLenght = 0;
+            string word = "";
+            foreach(string s in words)
+            {
+                if(s.Length > wordsLenght)
+                {
+                    wordsLenght = s.Length;
+                    word = s;
+                }
+            }
+            Console.WriteLine($"The longest word in the sentence is: {word}");
+        }
+    }
+}
